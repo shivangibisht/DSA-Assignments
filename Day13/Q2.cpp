9b079d2ae3a6fe68695c20e0227d1958b3a19948
shivangibisht <85785184+shivangibisht@users.noreply.github.com>
2021-07-15 09:40:14 +0530

    Create Q2.cpp

---
+++ Day13/Q2.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    vector<int> countBits(int n) {
+        vector<int>v(n+1);
+        v[0]=0;
+        for(int i=1;i<=n;i++){
+            v[i]=v[i&(i-1)]+1;
+        }
+        return v;
+    }
+};
